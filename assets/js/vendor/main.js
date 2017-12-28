ff9dcc2d792cdca2ee6c2814cc0d6f0058c6fb1b
Northerchild <osarus-13@hotmail.com>
2017-12-27 20:45:09 -0500

    Se agrega el slide

--- assets/js/vendor/main.js
+++ assets/js/vendor/main.js
@@ -1,9 +1,28 @@
 ;(function(){
 
 	let sticky = false
+	let currentPosition = 0
+	const imageCounter =  $("[data-name=image-counter]").attr("content")
+	console.log(imageCounter)
 	console.log($(window).height());
 	$("#sticky-navigation").removeClass("hidden")
 	$("#sticky-navigation").slideUp(0)
+
+	//Controlador del Slide
+	setInterval(()=>{
+		if (currentPosition < imageCounter) 
+		{
+			currentPosition++	
+		}
+		else
+		{
+			currentPosition = 0	
+		}
+		$("#gallery .inner").css({
+			left: "-"+currentPosition*100+"%"
+		});
+	},2500)
+
 	$(window).scroll(()=>{
 		const inBottom = isInBottom()
 
