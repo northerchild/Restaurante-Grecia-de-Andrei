ec4b68b1b014e1100e97d8a7b6e40830fd70322c
Northerchild <osarus-13@hotmail.com>
2018-01-04 20:33:34 -0500

    Se corrigen los bug en el SW y ahora la pagina es accesible sin Internet n_n

--- sw.js
+++ sw.js
@@ -1,3 +1,42 @@
+const CACHE_NAME = "Grecia-v3"
+const cache_urls = ["/offline/view.html","/offline/style.css","/assets/imgs/map.jpg"]
+
 self.addEventListener("install",function(e){
 	console.log("SW instalado")
+
+	caches.open(CACHE_NAME).then(function(cache){
+		console.log("Cache Open")
+		return cache.addAll(cache_urls)
+	})
+})
+
+self.addEventListener("activate", function(e){
+	e.waitUntil(
+			caches.key().then(function(cache_names){
+				return Promise.all(
+					cache_names.map(function(cache_names){
+						if (CACHE_NAME !== cache_names) {
+							return cache.delete(cache_names)//Esta promesa devuelve el cache
+						}
+					})
+					)
+			})
+		)
+})
+
+self.addEventListener("fetch",function(e){
+	e.respondWith(
+		caches.match(e.request).then(function(response){
+			if (response) {
+				console.log("Te estoy ahorrando una peticion :v")
+				return response;
+			}
+				return fetch(e.request)
+		}).catch(function(err){
+			if (e.request.mode == "navigate") 
+			{
+				return caches.match("/offline/view.html")
+			}
+		})
+		)
 })
